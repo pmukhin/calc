8c0b887cfa8652d4009d9d6cb01212948d0618e7
pmukhin <pauri.lentonen@gmail.com>
2025-02-13 18:47:23 +0100

    lacking test

--- src/eval.rs
+++ src/eval.rs
@@ -2,6 +2,7 @@ use crate::ast::Expr;
 
 pub struct Eval {}
 
+#[derive(Debug)]
 pub enum Error {}
 
 impl Eval {
@@ -17,5 +18,30 @@ impl Eval {
             Expr::Sub(l, r) => Ok(self.exec(l)? - self.exec(r)?),
             Expr::Div(l, r) => Ok(self.exec(l)? / self.exec(r)?),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_eval_mul() {
+        let expr = Expr::Mul(
+            Box::new(Expr::Add(Box::new(Expr::Number(5)), Box::new(Expr::Number(6)))),
+            Box::new(Expr::Sub(Box::new(Expr::Number(10)), Box::new(Expr::Number(3)))),
+        );
+        let result = Eval::new().exec(&expr);
+        assert_eq!(result.unwrap(), 77);
+    }
+
+    #[test]
+    fn test_eval_div() {
+        let expr = Expr::Div(
+            Box::new(Expr::Add(Box::new(Expr::Number(5)), Box::new(Expr::Number(6)))),
+            Box::new(Expr::Sub(Box::new(Expr::Number(10)), Box::new(Expr::Number(3)))),
+        );
+        let result = Eval::new().exec(&expr);
+        assert_eq!(result.unwrap(), 1);
     }
 }
